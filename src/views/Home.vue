a70743258a6e11aaeb6397ab6d753e9e1365effb
Moritz Miedler <miedler.m03@htlwienwest.at>
2021-10-15 14:38:18 +0200

    Nummer 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="display:flex">
+    <Sheet style="margin-left:20px">
+      <h3>Memo to myself</h3>
+      <p>Always prepare for PLFs in time!</p>
+      <img src="info.svg" width="80px" />
+    </Sheet>
+    <Sheet style="margin-left:20px">
+      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
+      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
+      <p>Wish i was there!</p>
+      <span style="font-style: italic">E. Wahn</span>
+    </Sheet>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Sheet from "@/components/Sheet.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Sheet
   }
-}
+};
 </script>
