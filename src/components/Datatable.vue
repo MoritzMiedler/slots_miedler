1bb9f21ef213b84b36ff06ac9e8b28f99c2f6fa1
Moritz Miedler <miedler.m03@htlwienwest.at>
2021-10-15 15:34:53 +0200

    Nummer3

---
+++ src/components/Datatable.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <table class="data-table">
+      <thead>
+        <tr>
+          <th v-for="(i, id) in headers" :key="id">{{ i }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(i, id) in items" :key="id">
+          <td>{{ i.name }}</td>
+          <td>{{ i.calories }}</td>
+          <td>{{ i.fat }}</td>
+          <td>{{ i.protein }}</td>
+          <td>{{ i.carbohydrates }}</td>
+          <td>{{ i.iron }}</td>
+        </tr>
+      </tbody>
+      <tr>
+        <slot name="bodyappend" />
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array
+    },
+    items: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped>
+.data-table {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 40px;
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.data-table td,
+th {
+  text-align: left;
+}
+.data-table footer {
+  margin: 30px 0 30px 0;
+  font-style: italic;
+}
+.data-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.data-table th,
+.data-table td {
+  padding: 12px 15px;
+}
+.data-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.data-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
       </Sheet>
     </div>
 
-    <div id="app" v-if="true">
+    <div id="app" v-if="false">
       <!-- Die Reihenfolge ist egal -->
       <AppLayout>
         <template v-slot:header>
@@ -30,6 +30,24 @@
         </template>
       </AppLayout>
     </div>
+    <div id="app">
+      <DataTable :items="desserts" :headers="headers">
+        <template v-slot:header>
+          <h2>Watch What You Eat!</h2>
+        </template>
+        <template v-slot:footer> Created 2021 by Robert Baumgartner </template>
+        <template v-slot:bodyappend>
+          <tr class="last-row">
+            <td>Total</td>
+            <td>{{ totalCalories }}</td>
+            <td>{{ totalFat }}</td>
+            <td>{{ totalCarbs }}</td>
+            <td>{{ totalProtein }}</td>
+            <td></td>
+          </tr>
+        </template>
+      </DataTable>
+    </div>
   </div>
 </template>
 
@@ -38,13 +56,107 @@
 import Sheet from "@/components/Sheet.vue";
 import AppLayout from "@/components/AppLayout.vue";
 import AppButton from "@/components/AppButton.vue";
+import DataTable from "@/components/Datatable.vue";
 
 export default {
   name: "Home",
   components: {
     Sheet,
     AppLayout,
-    AppButton
+    AppButton,
+    DataTable
+  },
+  data() {
+    return {
+      desserts: [
+        {
+          name: "pizza",
+          calories: 345,
+          fat: 6456,
+          protein: 23435,
+          carbohydrates: 34636,
+          iron: 235
+        },
+        {
+          name: "pizza2",
+          calories: 346346,
+          fat: 34534,
+          protein: 235,
+          carbohydrates: 346,
+          iron: 45734
+        },
+        {
+          name: "pizza3",
+          calories: 2364,
+          fat: 346,
+          protein: 45345,
+          carbohydrates: 3467457,
+          iron: 245346
+        },
+        {
+          name: "pizza4",
+          calories: 4574357,
+          fat: 4357347,
+          protein: 3473657,
+          carbohydrates: 4573437,
+          iron: 4357347
+        },
+        {
+          name: "pizza5",
+          calories: 543737,
+          fat: 437457,
+          protein: 4577,
+          carbohydrates: 4574357,
+          iron: 45745
+        },
+        {
+          name: "pizza6",
+          calories: 35675,
+          fat: 43534,
+          protein: 34635,
+          carbohydrates: 346345,
+          iron: 435
+        }
+      ],
+      headers: [
+        "Name",
+        "KCAL",
+        "FAT(G)",
+        "PROTEIN(G)",
+        "CARBOHYDATES(G)",
+        "IRON(%)"
+      ]
+    };
+  },
+  computed: {
+    totalCalories() {
+      let sum = 0;
+      this.desserts.forEach(element => {
+        sum += element.calories;
+      });
+      return sum;
+    },
+    totalProtein() {
+      let sum = 0;
+      this.desserts.forEach(element => {
+        sum += element.protein;
+      });
+      return sum;
+    },
+    totalCarbs() {
+      let sum = 0;
+      this.desserts.forEach(element => {
+        sum += element.carbohydrates;
+      });
+      return sum;
+    },
+    totalFat() {
+      let sum = 0;
+      this.desserts.forEach(element => {
+        sum += element.fat;
+      });
+      return sum;
+    }
   }
 };
 </script>
